fd632399a8bca9a9f36e855320b49c718e76287b
Kabelo Hlaka <kabelohlaka@gmail.com>
2023-08-22 04:10:17 -0700

    _printf

--- _printf.c
+++ _printf.c
@@ -1,7 +1,6 @@
 #include "main.h"
 #include <stdarg.h>
 #include <unistd.h>
-#include <stdio.h>
 /**
  * _putchar - Prints a char
  * @c: The character to be printed
@@ -12,10 +11,46 @@ int _putchar(char c)
 	return (write(1, &c, 1));
 }
 
+/**
+ * print_char - Prints a char
+ * @args: Converts int to char
+ * Return: Number of chars printed
+ */
+int print_char(va_list args)
+{
+	char c = (char) va_arg(args, int);
+
+	return (_putchar(c));
+}
+
+/**
+ * print_string - Prints an output string
+ * @args: Converts int to char
+ * Return: num of bytes printed
+ */
+int print_string(va_list args)
+{
+	char *str = va_arg(args, char *);
+	int count = 0;
+
+	if (str == NULL)
+	{
+		count += _printf("(null)");
+		count += 6;
+	}
+	else
+	{
+		while (*str)
+		{
+			count += _putchar(*str++);
+		}
+	}
+	return (count);
+}
 
 /**
  * _printf - Prints an output according to format
- * @format: format string
+ * @format: format specifier
  * Return: num of bytes printed
  */
 int _printf(const char *format, ...)
@@ -24,38 +59,34 @@ int _printf(const char *format, ...)
 	int count = 0;
 
 	if (format == NULL)
-		return (-1);
+	return (-1);
 	va_start(args, format);
-
-	for (; *format != '\0'; format++)
+	while (*format)
 	{
 		if (*format != '%')
 		{
-			count += _putchar(*format);
+			count += _putchar(*format++);
 			continue;
 		}
-
 		format++;
-		if (*format == '\0')
-			break;
-
-		if (*format == 'c')
-			count += _putchar((char)va_arg(args, int));
+		if (*format == '%')
+		{
+			count += _putchar('%');
+		}
+		else if (*format == 'c')
+		{
+			count += print_char(args);
+		}
 		else if (*format == 's')
 		{
-			char *str = va_arg(args, char *);
-
-			if (str == NULL)
-				str = "(null)";
-			while (*str != '\0')
-				count += _putchar(*str++);
+			count += print_string(args);
 		}
 		else
 		{
-			count += _putchar(*format);
+			count += _putchar('%') + _putchar(*format);
 		}
+		format++;
 	}
-
 	va_end(args);
 	return (count);
 }
