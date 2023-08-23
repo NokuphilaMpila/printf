827cdf131d7c29d0eb4611937e230a99c1050860
Kabelo Hlaka <kabelohlaka@gmail.com>
2023-08-22 19:43:51 -0700

    Task 1 functions.c

--- functions.c
+++ functions.c
@@ -35,7 +35,7 @@ int integer_printf(const char *format, ...)
 		else if (*format == 'd' || *format == 'i')
 		{
 			int num = va_arg(args, int);
-			count += print integer(num);
+			count += printf("%d" , num);
 		}
 		else
 		{
@@ -45,4 +45,21 @@ int integer_printf(const char *format, ...)
 	}
 	va_end(args);
 	return (count);
+}
+
+/**
+ * main - function that prints integers
+ * Return: 0
+ */
+int main() {
+
+		int num1 = 123;
+		int num2 = -456;
+		int count;
+
+		count = integer_printf("This is a test\n");
+		printf("Number of characters printed: %d\n", count);
+		count = integer_printf("The numbers are: %d and %i\n", num1, num2);
+		printf("Number of characters printed: %d\n", count);
+		return 0;
 }
